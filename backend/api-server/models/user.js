e9a8717ea61e95a8157c0bf9dd32d363a0547c9f
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-08-11 18:18:04 +0530

    Add mongoose models.

---
+++ backend/api-server/models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+  },
+  password: String,
+});
+
+const UserModel = mongoose.model("user", userSchema);
+module.exports = UserModel;
