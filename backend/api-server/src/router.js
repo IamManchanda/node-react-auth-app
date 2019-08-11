79381f6f4659503b7cf5b2e7156248096ce0c9c5
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-08-12 01:36:03 +0530

    Making an Authenticated request!

--- backend/api-server/src/router.js
+++ backend/api-server/src/router.js
@@ -1,6 +1,19 @@
+const passport = require("passport");
+
 const { signup } = require("./controllers/auth")
+const passportService = require("./services/passport");
+
+const requireAuth = passport.authenticate(
+  "jwt",
+  {
+    session: false,
+  },
+);
 
 const router = (app) => {
+  app.get("/", requireAuth, function handleHomeRouteGET(request, response) {
+    response.send({ hi: "there" });
+  })
   app.post("/signup", signup)
 };
 
